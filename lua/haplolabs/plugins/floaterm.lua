abc6063d778636470f8b69afff78a99c407d50b0
Nate Thompson <nate.thompson@maxar.com>
2022-09-05 06:26:35 -0600

    Added updated floaterm mappings

--- lua/haplolabs/plugins/floaterm.lua
+++ lua/haplolabs/plugins/floaterm.lua
@@ -3,9 +3,9 @@ local function init()
 
   local options = { noremap = true }
 
-  --vim.api.nvim_command([[
-    --tnoremap <ESC> <C-\><C-n>
-  --]])
+  -- vim.api.nvim_command([[
+  --   tnoremap <C-Shift-D> <C-\><C-n>
+  -- ]])
 
   map('n', '<leader>bb', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 btm<CR>', options)
   map('n', '<leader>k9', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 k9s<CR>', options)
@@ -14,6 +14,12 @@ local function init()
   map('n', '<leader>nn', '<CMD>FloatermNew --autoclose=2 --height=0.5 --width=0.5 nnn -Hde<CR>', options)
   map('n', '<leader>tt', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 zsh<CR>', options)
   map('n', '<leader>tw', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 taskwarrior-tui<CR>', options)
+  map('n', '<leader>,,', '<CMD>FloatermToggle<CR>', options)
+  map('n', '<leader>,.', '<CMD>FloatermNext<CR>', options)
+  map('n', '<leader>..', '<CMD>FloatermPrev<CR>', options)
+  map('t', '<leader>,,', '<C-\\><C-n><CMD>FloatermToggle<CR>', options)
+  map('t', '<leader>,.', '<C-\\><C-n><CMD>FloatermNext<CR>', options)
+  map('t', '<leader>..', '<C-\\><C-n><CMD>FloatermPrev<CR>', options)
 end
 
 return {
